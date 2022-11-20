17a3ab0131eb3defcdc47577db319fbc38a361d0
Yari <narducciyarigmail.com>
2022-11-20 21:05:50 +0100

    riscrittura codice

--- js/script.js
+++ js/script.js
@@ -1,133 +1,75 @@
-//Simon Says
-//Creo l'evento click
-let container = document.getElementById("small-container");
-let btn = document.querySelector("#btn");
-
-let casualNumArr =  genArrNumUnicRandomMinMax (5, 1, 100);
-let seconds = 5;
-// let inputField = document.getElementById("input-field");
-let timer = document.getElementById("timer");
-let inputBtn = document.querySelector("#numb-btn");
-let input = document.getElementById("input");
-let punteggio = casualNumArr.length;
-let risultato = [];
- let nuovoElemento = createBox("div","box");
- let numeroUtente ;
- let result = document.getElementById("title")
+const container = document.getElementById('container');
+const btn = document.getElementById('button-play');
+const input = document.getElementById('number');
+const btnResult = document.getElementById('btn-result');
+let result = document.getElementById('result');
+let arrayResult = [];
 
 
+btn.addEventListener('click', function () {
 
- console.log(casualNumArr);
+    // create new div e array
+    container.innerHTML = '';
+    let createDiv = document.createElement('div');
+    let numGame = createArrayNum(5, 1, 100);
+    createDiv.innerHTML = numGame;
+    container.append(createDiv);
+    console.log(numGame);
+    arrayResult = [];
+    result.innerHTML = '';
+    input.classList.add('hidden');
+    btnResult.classList.add('hidden');
+    let win;
 
-btn.addEventListener("click",
-function(event){
-    event.preventDefault;
-    container.innerHTML= "";
-
-   
-        
 
     setTimeout(() => {
         container.innerHTML = "";
-        input.style.display="block";
-        inputBtn.style.display="block";
-    }, 3000);
+        input.classList.remove('hidden');
+        btnResult.classList.remove('hidden');
+    }, 5000);
 
-   for (let i = 0; i < casualNumArr.length; i++) {
-   
-    let nuovoElemento = createBox("div","box");
-        nuovoElemento.innerHTML = i;
-        container.append(nuovoElemento);
-        nuovoElemento.innerHTML = casualNumArr[i];
-        
-        
+    btnResult.addEventListener('click', function () {
+        console.log(input.value);
+        arrayResult.push(input.value);
 
-    
-    console.log(nuovoElemento);
-  
-      
-       
+        if (5 === arrayResult.length) {
+            input.classList.add('hidden');
+            btnResult.classList.add('hidden');
+            win = true;
 
-    }
-   }
-  
-)
-inputBtn.addEventListener('click', function () {
-    console.log(input.value);
-   risultato.push(input.value);
-
-    if (5 === risultato.length) 
-        input.classList.add('hidden');
-        inputBtn.classList.add('hidden');
-        win = true;
-
-        for (let i = 0; i < casualNumArr.length; i++) {
-            if (casualNumArr[i] != risultato[i]) {
-                win = false
+            for (let i = 0; i < numGame.length; i++) {
+                if (numGame[i] != arrayResult[i]) {
+                    win = false
+                }
+                else {
+                    container.append(arrayResult[i] + '; ');
+                }
+            };
+            if (win) {
+                result.innerHTML = 'Hai vinto!'
+            } else if (!win) {
+                result.innerHTML = 'Hai perso!';
             }
-            else {
-                container.append(risultato[i] + '; ');
-            }
-        };
-        if (win) {
-            result.innerHTML = 'Hai vinto!'
-        } else if (!win) {
-            result.innerHTML = 'Hai perso!';
         }
     })
-// Creiamo un Array di 5 numeri casuali
+});
 
 
 
+// function
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function randomNumberMinMax (min, max) {
-    return ( Math.floor(Math.random() * ((max + 1) - min) + min));
+// funzionrandom number
+function randomNumber(min, max) {
+    let randomNum = Math.floor(Math.random() * ((max + 1) - min) + min);
+    return randomNum
 }
 
-// creo la funzione per creare un arrey di numeri random unici 
-function genArrNumUnicRandomMinMax (maxElement, minNum, maxNum) {
-    
-    // creo l'array
-    const arrNumUnicRandom = [];
-
-    // genero i numeri da inserire nell'array
-    while (arrNumUnicRandom.length < maxElement) {
-        let newNumber = randomNumberMinMax (minNum, maxNum);
-        if (!arrNumUnicRandom.includes(newNumber)){
-            arrNumUnicRandom.push(newNumber);
-        }
+function createArrayNum(numPossibility, minNum, maxNum) {
+    let arrayNumb = [];
+    while (arrayNumb.length < numPossibility) {
+        let randomNumForArray = randomNumber(minNum, maxNum);
+        if (!arrayNumb.includes(randomNumForArray))
+            arrayNumb.push(randomNumForArray);
     }
-
-    // ritorno l'array generato
-    return arrNumUnicRandom;
-}
-function createBox(typeElement, idClassElement){
-    const divElement = document.createElement("div");
-    divElement.classList.add("box");
-   
-
-
-    return divElement;
-}
+    return arrayNumb;
+};
